87620c90f19bffa26e62f30b92ab1a8fc803ff8e
nakainu <c011619379@edu.teu.ac.jp>
2019-01-31 11:42:03 +0900

    fixed

--- newlang4/ForNode.java
+++ newlang4/ForNode.java
@@ -8,76 +8,76 @@ import java.util.Set;
 import newlang3.*;
 
 public class ForNode extends Node {
-	Node init;
-	Node max;
-	Node symbol;
-	String step;
-	
-	private final static Set<LexicalType> FIRST = new HashSet<>(Arrays.asList(
-			LexicalType.FOR
-		));	
-	public static boolean isMatch(LexicalType type) {
-		return FIRST.contains(type);
-	}
-	
-	private ForNode(Environment env) {
-		super.env = env;
-		type = NodeType.FOR_STMT;
-	}
-	
-	// 次に出てくる字句をparseできるnodeをひとつ返す
-	public static Node getHandler(Environment env) throws Exception {
-		return new ForNode(env);
-	}
-	
-	public void parse() throws Exception {
-		if(env.getInput().get().getType() != LexicalType.FOR) throw new Exception("ForNodeのforのparse部分がおかしいエラー");
+Node init;
+Node max;
+Node symbol;
+String step;
 
-		if(SubstNode.isMatch(env.getInput().peek(1).getType())) {
-			init = SubstNode.getHandler(env);
-			init.parse();
-		}else {
-			throw new Exception("ForNodeのsubstの部分がおかしいエラー");
-		}
-		if(env.getInput().peek(1).getType() == LexicalType.TO) {
-			env.getInput().get();
-		} else {
-			throw new Exception("ForNodeのTOの部分がおかしいエラー");
-		}
-		if(env.getInput().peek(1).getType() == LexicalType.INTVAL) {
-			max = ConstNode.getHandler(env);
-			max.parse();
-		} else {
-			throw new Exception("ForNodeのintvalの部分がおかしいエラー");
-		}
-		if(env.getInput().peek(1).getType() == LexicalType.NL) {
-			env.getInput().get();
-		} else {
-			throw new Exception("ForNodeのNLの部分がおかしいエラー");
-		}
-		if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
-			symbol = StmtListNode.getHandler(env);
-			symbol.parse();
-		} else {
-			throw new Exception("ForNodeのstmt_listの部分がおかしいエラー");
-		}
-		System.out.println(env.getInput().peek(1).getType());
-		if(env.getInput().peek(1).getType() == LexicalType.NEXT) {
-			env.getInput().get();
-		} else {
-			throw new Exception("ForNodeのNEXTの部分がおかしいエラー");
-		}
-		if(env.getInput().peek(1).getType() == LexicalType.NAME) {
-			step = env.getInput().get().getValue().getSValue();
-		} else {
-			throw new Exception("ForNodeのNAMEの部分がおかしいエラー");
-		}
+private final static Set<LexicalType> FIRST = new HashSet<>(Arrays.asList(
+		LexicalType.FOR
+	));
+public static boolean isMatch(LexicalType type) {
+	return FIRST.contains(type);
+}
+
+private ForNode(Environment env) {
+	super.env = env;
+	type = NodeType.FOR_STMT;
+}
+
+// 次に出てくる字句をparseできるnodeをひとつ返す
+public static Node getHandler(Environment env) throws Exception {
+	return new ForNode(env);
+}
+
+public void parse() throws Exception {
+	if(env.getInput().get().getType() != LexicalType.FOR) throw new Exception("ForNodeのforのparse部分がおかしいエラー");
+
+	if(SubstNode.isMatch(env.getInput().peek(1).getType())) {
+		init = SubstNode.getHandler(env);
+		init.parse();
+	}else {
+		throw new Exception("ForNodeのsubstの部分がおかしいエラー");
 	}
-	
-	public String toString() {
-		String str = "";
-		str += String.format("FOR MAX STMTLIST STEP" + init, max, symbol, step);
-		return str;
+	if(env.getInput().peek(1).getType() == LexicalType.TO) {
+		env.getInput().get();
+	} else {
+		throw new Exception("ForNodeのTOの部分がおかしいエラー");
 	}
+	if(env.getInput().peek(1).getType() == LexicalType.INTVAL) {
+		max = ConstNode.getHandler(env);
+		max.parse();
+	} else {
+		throw new Exception("ForNodeのintvalの部分がおかしいエラー");
+	}
+	if(env.getInput().peek(1).getType() == LexicalType.NL) {
+		env.getInput().get();
+	} else {
+		throw new Exception("ForNodeのNLの部分がおかしいエラー");
+	}
+	if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
+		symbol = StmtListNode.getHandler(env);
+		symbol.parse();
+	} else {
+		throw new Exception("ForNodeのstmt_listの部分がおかしいエラー");
+	}
+	if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
+	if(env.getInput().peek(1).getType() == LexicalType.NEXT) {
+		env.getInput().get();
+	} else {
+		throw new Exception("ForNodeのNEXTの部分がおかしいエラー");
+	}
+	if(env.getInput().peek(1).getType() == LexicalType.NAME) {
+		step = env.getInput().get().getValue().getSValue();
+	} else {
+		throw new Exception("ForNodeのNAMEの部分がおかしいエラー");
+	}
+}
+
+public String toString() {
+	String str = "";
+	str += String.format("FOR MAX STMTLIST STEP" + init, max, symbol, step);
+	return str;
+}
 
 }
--- newlang4/IfBlockNode.java
+++ newlang4/IfBlockNode.java
@@ -11,32 +11,56 @@ import java.util.List;
 import newlang3.LexicalType;
 
 public class IfBlockNode extends Node {
-	List<Node> cond = new ArrayList<>();
-	List<Node> trueprocess = new ArrayList<>();
-	Node elseprocess = null;
+List<Node> cond = new ArrayList<>();
+List<Node> trueprocess = new ArrayList<>();
+Node elseprocess = null;
 
-	private final static Set<LexicalType> FIRST = new HashSet<>(Arrays.asList(
-			LexicalType.IF
-			));	
+private final static Set<LexicalType> FIRST = new HashSet<>(Arrays.asList(
+		LexicalType.IF
+		));
 
-	public static boolean isMatch(LexicalType type) {
-		return FIRST.contains(type);
+public static boolean isMatch(LexicalType type) {
+	return FIRST.contains(type);
+}
+
+private IfBlockNode(Environment env) {
+	super.env = env;
+	type = NodeType.IF_BLOCK;
+}
+
+// 次に出てくる字句をparseできるnodeをひとつ返す
+public static Node getHandler(Environment env) throws Exception {
+	return new IfBlockNode(env);
+}
+
+public void parse() throws Exception {
+	if(env.getInput().get().getType() != LexicalType.IF) {
+		throw new Exception("IFじゃないよ");
 	}
 
-	private IfBlockNode(Environment env) {
-		super.env = env;
-		type = NodeType.IF_BLOCK;
+	if(CondNode.isMatch(env.getInput().peek(1).getType())) {
+		Node handler = null;
+		handler = CondNode.getHandler(env);
+		handler.parse();
+		cond.add(handler);
 	}
 
-	// 次に出てくる字句をparseできるnodeをひとつ返す
-	public static Node getHandler(Environment env) throws Exception {
-		return new IfBlockNode(env);
+	if(env.getInput().get().getType() != LexicalType.THEN) {
+		throw new Exception("THENじゃないよ");
 	}
 
-	public void parse() throws Exception {
-		if(env.getInput().get().getType() != LexicalType.IF) {
-			throw new Exception("IFじゃないよ");
-		}
+	if(env.getInput().get().getType() != LexicalType.NL) {
+		throw new Exception("NLじゃないよ");
+	}
+	if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
+		Node handler = null;
+		handler = StmtListNode.getHandler(env);
+		handler.parse();
+		trueprocess.add(handler);
+	}
+	if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
+	while (env.getInput().peek(1).getType() == LexicalType.ELSEIF) {
+		env.getInput().get();
 
 		if(CondNode.isMatch(env.getInput().peek(1).getType())) {
 			Node handler = null;
@@ -58,57 +82,35 @@ public class IfBlockNode extends Node {
 			handler.parse();
 			trueprocess.add(handler);
 		}
-		while (env.getInput().peek(1).getType() == LexicalType.ELSEIF) {
-			env.getInput().get();
-			
-			if(CondNode.isMatch(env.getInput().peek(1).getType())) {
-				Node handler = null;
-				handler = CondNode.getHandler(env);
-				handler.parse();
-				cond.add(handler);
-			}
-
-			if(env.getInput().get().getType() != LexicalType.THEN) {
-				throw new Exception("THENじゃないよ");
-			}
-
-			if(env.getInput().get().getType() != LexicalType.NL) {
-				throw new Exception("NLじゃないよ");
-			}
-			if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
-				Node handler = null;
-				handler = StmtListNode.getHandler(env);
-				handler.parse();
-				trueprocess.add(handler);
-			}
-		}
-		if (env.getInput().peek(1).getType() == LexicalType.ELSE) {
-			env.getInput().get();
+		if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
+	}
+	if (env.getInput().peek(1).getType() == LexicalType.ELSE) {
+		env.getInput().get();
 
 
-			if(env.getInput().get().getType() != LexicalType.NL) {
-				throw new Exception("NLじゃないよ");
-			}
-			if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
-				Node handler = null;
-				handler = StmtListNode.getHandler(env);
-				handler.parse();
-				elseprocess = handler;
-			}
-		}
-		if(env.getInput().get().getType() != LexicalType.ENDIF) throw new Exception("ENDIFじゃないよ");
 		if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
 
-	}
-
-	public String toString() {
-		String str = "";
-		str += String.format("IF THEN" + cond, trueprocess);
-		
-		if(elseprocess != null) {
-			str += String.format("ELSE" + elseprocess);
+		if(StmtListNode.isMatch(env.getInput().peek(1).getType())) {
+			Node handler = null;
+			handler = StmtListNode.getHandler(env);
+			handler.parse();
+			elseprocess = handler;
 		}
-		return str;
-		
+		if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
 	}
+	if(env.getInput().get().getType() != LexicalType.ENDIF) throw new Exception("ENDIFじゃないよ");
+	if(env.getInput().get().getType() != LexicalType.NL) throw new Exception("NLじゃないよ");
+
+}
+
+public String toString() {
+	String str = "";
+	str += String.format("IF THEN" + cond, trueprocess);
+
+	if(elseprocess != null) {
+		str += String.format("ELSE" + elseprocess);
+	}
+	return str;
+
+}
 }
